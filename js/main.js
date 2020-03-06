104c67a9f41020c2020c8dbece4e352233523dbd
Alina Bychkova <ialina240200@gmail.com>
2020-03-06 12:27:19 +0700

    Disable resize event

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-const shapesElement = document.querySelector('.shapes');
+// const shapesElement = document.querySelector('.shapes');
 
-function handleResize() {
-  if (this.innerWidth / this.innerHeight >= 16 / 9) {
-    shapesElement.style.height = "calc(100vw*9/16)"; 
-    shapesElement.style.width = "100vw"
-  } else {
-    shapesElement.style.height = "100vh"; 
-    shapesElement.style.width = "calc(100vh*16/9)"
-  }
-}
-handleResize()
-window.addEventListener("resize", handleResize)
+// function handleResize() {
+//   if (window.matchMedia("(min-width: 480px)").matches) {
+//     desktopResize()
+//   } else {
+//     /* the viewport is less than 400 pixels wide */
+//   }
+// }
+// function desktopResize() {
+//   if (this.innerWidth / this.innerHeight >= 16 / 9) {
+//     shapesElement.style.height = "calc(100vw*9/16)"; 
+//     shapesElement.style.width = "100vw"
+//   } else {
+//     shapesElement.style.height = "100vh"; 
+//     shapesElement.style.width = "calc(100vh*16/9)"
+//   }
+// }
+// function mobileResize() {
+//   if () {
+
+//   } else {
+
+//   }
+// }
+// handleResize()
+// window.addEventListener("resize", handleResize)
